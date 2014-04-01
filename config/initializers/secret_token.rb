491515cb71630111c3425640dee3a6922eb0e614
stujo <wordpress@imagescript.com>
2014-04-01 15:44:55 -0700

    Template with pgres (reauires createuser -s -r roomtemp_ng and bundle exec rake db:create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoomtempAngular::Application.config.secret_key_base = '6587508be0bb2be6569ad92cbfeb63b3f6f9130aff47e1d9b84255425a1eb46019f419aab277516ccb2971f4e847640f78b3ee1c084732b0651be309d7e64809'
